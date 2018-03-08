78558c644be466a3ed6334ef9fb09798f72b4c57
github username <github email address>
2018-03-08 11:38:53 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,8 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+  # after_action :activate_student_path, only: :edit
+  protect_from_forgery with: :null_session
+
   def index
     @students = Student.all
   end
@@ -8,9 +10,34 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate
+    set_student
+
+    if @student.active
+      @student.update(active: false)
+    else
+      @student.update(active: true)
+    end
+    redirect_to "/students/#{@student.id}"
+  end
+
   private
+    # def activate_student_path
+    #   if !@student.active
+    #     @student.update(active: false)
+    #   else
+    #     @student.update(active: true)
+    #   end
+    #
+    #   redirect_to "/students/#{params["id"]}"
+    # end
 
     def set_student
       @student = Student.find(params[:id])
+      if @student.active == true
+        @student_active = "This student is currently active."
+      else
+        @student_active = "This student is currently inactive."
+      end
     end
 end
